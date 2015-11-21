2b97dad6d61bea87d242af031ae3916a4170ae98
Matthew Lam <lamm3@students.wwu.edu>
2015-11-20 20:22:55 -0800

    Added Rabin Miller primality test

--- key_exchange.py
+++ key_exchange.py
@@ -55,7 +55,29 @@ def connect_to_server(ip, port):
 
 # AES msg exchange
 # def secure_message(input):
-
+#Rabin Miller
+def RabinMiller(n, k = 7):
+    if n < 6: 
+        return [False, False, True, True, False, True][n]
+    elif n & 1 == 0: 
+        return False
+    else:
+        s, d = 0, n - 1
+        while d & 1 == 0:
+        s, d = s + 1, d >> 1
+        for a in random.sample(xrange(2, min(n - 2, sys.maxint)), min(n - 4, k)):
+            x = pow(a, d, n)
+            if x != 1 and x + 1 != n:
+                for r in xrange(1, s):
+                    x = pow(x, 2, n)
+                    if x == 1:
+                        return False
+                    elif x == n - 1:
+                        a = 0  
+                        break  
+                if a:
+                    return False  
+      return True
 #Returns a random prime number
 def get_random_prime():
     random_generator = Crypto.Random.new().read
