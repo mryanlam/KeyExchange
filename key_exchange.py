8af069061d575ecdcb2c09308cb9fcec9624d28c
Matthew Lam <mryanlam@outlook.com>
2015-11-23 21:05:03 -0800

    Added some new methods, but still untested.

--- key_exchange.py
+++ key_exchange.py
@@ -3,26 +3,24 @@ import sys
 import Crypto # https://www.dlitz.net/software/pycrypto/
 import socket
 import argparse
+import json
 
 def main(args):
     parse = argparse.ArgumentParser()
     parse.add_argument('-s', '--isServer', type = int, default = True, required = True)
     parse.add_argument('-i', '--ip', type = str)
     parse.add_argument('-p', '--port', type = int, required = True)
-    
+
     args = parse.parse_args()
     isServer = args.isServer
     ip = args.ip
     port = args.port
-   
+
     if isServer == 0:
-        #key stuff
-        #p = get_random_prime()
-        
         start_server(port)
-        
     else:
         connect_to_server(ip, port)
+
 
 # Listen for a connection (Server)
 # http://stackoverflow.com/questions/7749341/very-basic-python-client-socket-example
@@ -36,17 +34,46 @@ def start_server(port):
         if len(buf) > 0:
             print(buf)
             break
+     p = 0
+     prime = False
+     while !prime:
+         p = random.
+         prime = RabinMiller(p)
+     alph = generator(p)
+     a = random.randrange(1, p - 1)
+     beta = buildkey(alph, a, p)
+     public_key = dict()
+     public_key[alph] = alph
+     public_key[beta] = beta
+     public_key[p] = p
+     json_pub_key = json.dumps(public_key)
+     serversocket.send(json_pub_key)
+     #get encrypted aes key as json_aes_key
+     #AESkey = decrypt(json_aes_key[y1], json_aes_key[y2], p, alph, beta, a)
+
 
 # Connect to a server (Client)
 def connect_to_server(ip, port):
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientsocket.connect((ip, port))
     clientsocket.send('hello')
+    json_pub_key = clientsocket.recv(128)
 
+    #get (p, alpha, beta)
+    # download json from server and unpack it
+    public_key = json.loads(json_pub_key)
+    k = random.randrange(1, public_key[p] - 1)
+    AESkey = random.getrandbits(128)
+    y1 = gety1(public_key[p], public_key[alph], k)
+    y2 = gety2(public_key[p], public_key[beta], k, AESkey)
+    AES_message = dict()
+    AES_message[y1] = y1
+    AES_message[y2] = y2
+    #send encrypted key to p1
+    clientsocket.send(json.dumps(AES_message))
 
 # p = prime number, alph = generator
-def buildkey(alph, p):
-    a = random.randrange(1, p - 1)
+def buildkey(alph, a, p):
     beta = math.exp(alph, a)
     beta = beta % p
     return beta
@@ -54,30 +81,45 @@ def buildkey(alph, p):
 # Key Exchange
 
 # Encrypt with key ElGamal
-def encrypt(p, alph, beta):
+def gety1(p, alph, k):
     k = random.randrange(1, p - 1)
-    AESkey = random.getrandbits(128)
     y1 = math.exp(alph, k)
     y1 = y1 % p
+    return y1
+
+def gety2(p, beta, k, AESkey)
     y2 = AESkey * math.exp(beta, k)
     y2 = y2 % p
-    # send msg
+    return y2
 
 # Decrypt with key ElGamal
 def decrypt(y1, y2, p, alph, beta, a):
-    AESkey = math.exp(y1, a) % p
-    AESkey = math.exp(AESkey, -1) * y2
+    AESkey = extended_euclid(y1 * a,p) * y2
     AESkey = AESkey % p
     return AESkey
-    
+
+# Calculates the multiplicative inverse
+def inverse(a, m):
+    g, x, y = extended_euclid(a, m)
+    if g == 1:
+        return x % m
+
+def extended_euclid(a, b):
+    prevx, x = 1, 0; prevy, y = 0, 1
+    while b:
+        q = a/b
+        x, prevx = prevx - q*x, x
+        y, prevy = prevy - q*y, y
+        a, b = b, a % b
+    return a, prevx, prevy
 
 # AES msg exchange
 # def secure_message(input):
 #Rabin Miller
 def RabinMiller(n, k = 7):
-    if n < 6: 
+    if n < 6:
         return [False, False, True, True, False, True][n]
-    elif n & 1 == 0: 
+    elif n & 1 == 0:
         return False
     else:
         s, d = 0, n - 1
@@ -91,18 +133,18 @@ def RabinMiller(n, k = 7):
                     if x == 1:
                         return False
                     elif x == n - 1:
-                        a = 0  
-                        break  
+                        a = 0
+                        break
                 if a:
-                    return False  
+                    return False
     return True
-      
+
 #Find Generator
 def Generator(p):
     k = p - 1
     while True:
         alph = random.randrange(1, p)
-        found = True   
+        found = True
         for a in range(1, p):
             # make sure int?
             exp = k / a
@@ -113,7 +155,7 @@ def Generator(p):
         if found:
            return alph
     return 0
-        
+
 
 #Returns a random prime number
 def get_random_prime():
