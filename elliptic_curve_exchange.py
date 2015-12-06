1bdc1bf84b7574022ea2e8c65bb936f217816bc5
paulolason@gmail.com <olasonp2@quest-OptiPlex-9020>
2015-12-06 15:09:05 -0800

    testing only pos alphX values in elliptic, moddified padding method

--- elliptic_curve_exchange.py
+++ elliptic_curve_exchange.py
@@ -47,17 +47,17 @@ def start_server(port, pSize):
             b = 8
             print('Using curve y^2 = x^3 + ' + str(a) + 'x + ' + str(b))
             # find an alpha that is in the curve
-            #alphX = random.randrange(-20, 20)
-            #alphY = 0
-            #while True:
-            #    z = get_z(alphX, a, b, p)
-            #    if z != -1:
-            #        alphY = int(math.sqrt(z))
-            #        break
-            #    else:
-            #        alphX = alphX + 1;
-            alphX = -6
-            alphY = 206
+            alphX = random.randrange(0, 20)
+            alphY = 0
+            while True:
+                z = get_z(alphX, a, b, p)
+                if z != -1:
+                    alphY = int(math.sqrt(z))
+                    break
+                else:
+                    alphX = alphX + 1;
+            #alphX = -6
+            #alphY = 206
             print('Chose alpha = (' + str(alphX) + ', ' + str(alphY) + ')')
             privKey = random.randrange(1, 20)
             print('Private key is ' + str(privKey))
